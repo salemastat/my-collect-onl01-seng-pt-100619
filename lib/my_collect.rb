a107dc92641b1e203152fa610ff3f0acdea5edda
Salem Asrat <salemasrat@gmail.com>
2019-10-18 20:04:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,11 @@
-my_collect(collection)
-i = 0 
- while i < collection.length
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
  
 
